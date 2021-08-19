a3b26bb0c33e5d927885cdf82828fd46e1384fcd
Eddy Martínez <emartinez@restrata.com>
2021-08-19 14:35:18 +0100

    Adds a Vue frontend project.

---
+++ vue-boot/frontend/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <hi> {{ title }} </hi>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      title: ''
+    }
+  },
+  mounted () {
+    fetch('http:localhost:8085')
+      .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        this.title = data
+      })
+  }
+}
+</script>
